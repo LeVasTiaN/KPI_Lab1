c221cc050a43690634491456f7f435bbf0103a16
Docvoids <143121183+Docvoids@users.noreply.github.com>
2025-06-13 20:28:48 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+    "encoding/json"
     "net/http"
+    "time"
 )
 
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+    currentTime := time.Now().Format(time.RFC3339)
+    response := map[string]string{"time": currentTime}
+
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(response)
+}
+
 func main() {
+    http.HandleFunc("/time", timeHandler)
     http.ListenAndServe(":8795", nil)
 }
